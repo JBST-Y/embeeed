90c9523e5a5decbf9e416d339b7ca12808ddcda6
yubin lee <lyb9969@naver.com>
2022-12-07 13:55:56 +0900

    yubin
    led

--- project/ledonoff.ino
+++ project/ledonoff.ino
@@ -44,161 +44,68 @@ void loop() {
   client.flush();
 
    Serial.println(WiFi.localIP());
-
-  int val=0;
   
     if (req.indexOf("/gpio/0") != -1){
-    val=val+1;  
-    if (val==1){
     pinMode(15, OUTPUT);
     pinMode(5, OUTPUT);
-    digitalWrite(0, 0);
-    digitalWrite(3, 0);
-    digitalWrite(4, 0); 
+    digitalWrite(2, 0);
     digitalWrite(5, 1);
-    digitalWrite(9, 0);
-    digitalWrite(10, 0); 
-    digitalWrite(12, 0);
     digitalWrite(13, 0);
-    digitalWrite(14, 0);
     digitalWrite(15, 1); 
-    digitalWrite(16, 0);
-    Serial.println("1");
+    digitalWrite(16, 0); 
   }   
-    else if (val==2){  
+    else if (req.indexOf("/gpio/1") != -1){  
     pinMode(15, OUTPUT);
     pinMode(16, OUTPUT); 
-    digitalWrite(0, 0);
-    digitalWrite(3, 0);
-    digitalWrite(4, 0); 
     digitalWrite(5, 0);
-    digitalWrite(9, 0);
-    digitalWrite(10, 0); 
-    digitalWrite(12, 0);
-    digitalWrite(13, 0);
-    digitalWrite(14, 0);
-    digitalWrite(15, 1); 
     digitalWrite(16, 1);
-    Serial.println("2");
   }
-    else if (val==3){
-    pinMode(4, OUTPUT);
+    else if (req.indexOf("/gpio/2") != -1){
+    pinMode(1, OUTPUT);
     pinMode(15, OUTPUT);
-    digitalWrite(0, 0);
-    digitalWrite(3, 0);
-    digitalWrite(4, 1); 
-    digitalWrite(5, 0);
-    digitalWrite(9, 0);
-    digitalWrite(10, 0); 
-    digitalWrite(12, 0);
-    digitalWrite(13, 0);
-    digitalWrite(14, 0);
-    digitalWrite(15, 1); 
+    digitalWrite(1, 1); 
     digitalWrite(16, 0);
-    Serial.println("3");
   }
-    else if (val==4){
-    pinMode(0, OUTPUT);
+    else if (req.indexOf("/gpio/3") != -1){
+    pinMode(2, OUTPUT);
     pinMode(12, OUTPUT);
-    digitalWrite(0, 1);
-    digitalWrite(3, 0);
-    digitalWrite(4, 0); 
-    digitalWrite(5, 0);
-    digitalWrite(9, 0);
-    digitalWrite(10, 0); 
+    digitalWrite(2, 1);
+    digitalWrite(1, 0); 
     digitalWrite(12, 1);
-    digitalWrite(13, 0);
-    digitalWrite(14, 0);
     digitalWrite(15, 0); 
-    digitalWrite(16, 0);
-    Serial.println("4");
-  }
-    else if (val==5){
-    pinMode(0, OUTPUT);
-    pinMode(14, OUTPUT);
-    digitalWrite(0, 1);
-    digitalWrite(3, 0);
-    digitalWrite(4, 0); 
-    digitalWrite(5, 0);
-    digitalWrite(9, 0);
-    digitalWrite(10, 0); 
-    digitalWrite(12, 0);
-    digitalWrite(13, 0);
-    digitalWrite(14, 1);
-    digitalWrite(15, 0); 
-    digitalWrite(16, 0);
-    Serial.println("5");
-  }
-    else if (val==6){
-    pinMode(0, OUTPUT);
-    pinMode(13, OUTPUT);
-    digitalWrite(0, 1);
-    digitalWrite(3, 0);
-    digitalWrite(4, 0); 
-    digitalWrite(5, 0);
-    digitalWrite(9, 0);
-    digitalWrite(10, 0); 
-    digitalWrite(12, 0);
-    digitalWrite(13, 1);
-    digitalWrite(14, 0);
-    digitalWrite(15, 0); 
-    digitalWrite(16, 0);
-    Serial.println("6");
-    val=0;
-   }
-  }
-    else if (req.indexOf("/gpio/1") != -1){
-    pinMode(3, OUTPUT);
-    digitalWrite(0, 0);
-    digitalWrite(4, 0); 
-    digitalWrite(5, 0);
-    digitalWrite(6, 0);
-    digitalWrite(3, 1); 
-    digitalWrite(8, 0);
-    digitalWrite(9, 0);
-    digitalWrite(10, 0); 
-    digitalWrite(11, 0);
-    digitalWrite(12, 0);
-    digitalWrite(13, 0);
-    digitalWrite(14, 0);
-    digitalWrite(15, 0); 
-    digitalWrite(16, 0);
-  }
-  else if (req.indexOf("/gpio/2") != -1){
-    pinMode(9, OUTPUT);
-    digitalWrite(0, 0);
-    digitalWrite(4, 0); 
-    digitalWrite(5, 0);
-    digitalWrite(6, 0);
-    digitalWrite(7, 0); 
-    digitalWrite(8, 0);
-    digitalWrite(9, 1);
-    digitalWrite(10, 0); 
-    digitalWrite(11, 0);
-    digitalWrite(12, 0);
-    digitalWrite(13, 0);
-    digitalWrite(14, 0);
-    digitalWrite(15, 0); 
-    digitalWrite(16, 0);
-  }
-  else if (req.indexOf("/gpio/3") != -1){
-    pinMode(10, OUTPUT);
-    digitalWrite(0, 0);
-    digitalWrite(4, 0); 
-    digitalWrite(5, 0);
-    digitalWrite(6, 0);
-    digitalWrite(7, 0); 
-    digitalWrite(8, 0);
-    digitalWrite(9, 0);
-    digitalWrite(10, 1); 
-    digitalWrite(3, 0);
-    digitalWrite(12, 0);
-    digitalWrite(13, 0);
-    digitalWrite(14, 0);
-    digitalWrite(15, 0); 
-    digitalWrite(16, 0);
+
   }
     else if (req.indexOf("/gpio/4") != -1){
+    pinMode(2, OUTPUT);
+    pinMode(14, OUTPUT);
+    digitalWrite(12, 0);
+    digitalWrite(14, 1);
+  }
+    else if (req.indexOf("/gpio/5") != -1){
+    pinMode(2, OUTPUT);
+    pinMode(13, OUTPUT);
+    digitalWrite(13, 1);
+    digitalWrite(14, 0);
+  }
+    else if (req.indexOf("/gpio/6") != -1){
+    pinMode(3, OUTPUT);
+    digitalWrite(3, 1); 
+    digitalWrite(9, 0);
+    digitalWrite(10, 0); 
+  }
+  else if (req.indexOf("/gpio/7") != -1){
+    pinMode(9, OUTPUT);
+    digitalWrite(3, 0); 
+    digitalWrite(9, 1);
+    digitalWrite(10, 0); 
+  }
+  else if (req.indexOf("/gpio/8") != -1){
+    pinMode(10, OUTPUT);
+    digitalWrite(3, 1); 
+    digitalWrite(9, 0);
+    digitalWrite(10, 0); 
+  }
+    else if (req.indexOf("/gpio/9") != -1){
     pinMode(9, OUTPUT);
     pinMode(15, OUTPUT);
     pinMode(16, OUTPUT);
@@ -217,10 +124,6 @@ void loop() {
     digitalWrite(15, 1); 
     digitalWrite(16, 1);
   }
-
-
-
-  
   else {
     Serial.println("invalid request");
     client.stop();
